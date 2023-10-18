4b38dd3d0b20ab2efe043e6a722d641ea3bdbdba
jw0202058 <jw0202058@korea.ac.kr>
2023-10-18 16:05:24 +0900

    feat(clue): random position, clue type

---
+++ Assets/Scripts/Play/ClueManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 단서 프리팹에 붙어 있음
+// 게임 중에서 변경 발생
+
+// 단서 숨기기, 단서 획득하기
+// 각각의 인스턴스에 붙어 있는 Clue를 받아옴
+
+public class ClueManager : MonoBehaviour
+{
+    public Clue Clue;
+
+    public void HideClue()
+    {
+
+    }
+
+    
+
+}
