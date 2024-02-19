373147d8e949103d7a55c43779360b0adc0f2c97
kyeah <dmobtxx19@gmail.com>
2024-02-19 18:24:19 +0900

    feat: add attack button to colloc

--- Assets/Scripts/Play/UI/CoolTimeUI.cs
+++ Assets/Scripts/Play/UI/CoolTimeUI.cs
@@ -4,14 +4,10 @@ using UnityEngine.UI;
 public class CoolTimeUI : MonoBehaviour
 {
     public Image cooltimeBar;
-    private GameObject cooltimeText;
-    private Text text;
 
     private void Start()
     {
         cooltimeBar = transform.GetChild(0).GetComponent<Image>();
-        cooltimeText = UIManager.Instance.CoolTimeText;
-        text = cooltimeText.transform.GetComponent<Text>();
 
         Init_UI();
         cooltimeBar.fillAmount = 0;
@@ -31,15 +27,6 @@ public class CoolTimeUI : MonoBehaviour
         if (cooltimeBar != null)
         {
             cooltimeBar.fillAmount = 1 - _fill;
-            if (sec > 0)
-            {
-                cooltimeText.SetActive(true);
-                text.text = sec.ToString();
-            }
-            else
-            {
-                cooltimeText.SetActive(false);
-            }
         }
     }
 }
