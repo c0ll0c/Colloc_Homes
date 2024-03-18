f422e7ed5dae85d0d66c2abcb53e0790e8baf239
asjay <sejin1314@korea.ac.kr>
2024-03-18 23:01:09 +0900

    feat: change minigames to scenes

--- Assets/Scripts/Models/PlayEvent.cs
+++ Assets/Scripts/Models/PlayEvent.cs
@@ -1,7 +1,11 @@
+using UnityEngine.SceneManagement;
+
 public interface IPlayEvent
 {
     DistractionController Distraction { get; set; }
     EventSolutionHandler Solution { get; set; }
+    string SceneName { get; set; }
+
     void SetDistractionController(DistractionController _dc)
     {
         Distraction = _dc;
@@ -12,10 +16,15 @@ public interface IPlayEvent
         Solution.SetClickable();
     }
     string Play();
-    void MiniGame();
+    void MiniGame()
+    {
+        Distraction.gameObject.SetActive(false);
+        SceneManager.LoadScene(SceneName, mode: LoadSceneMode.Additive);
+    }
     void CloseMiniGame(bool _clear)
     {
         Distraction.gameObject.SetActive(true);
+        SceneManager.UnloadSceneAsync(SceneName);
         if (_clear) Clear();
     }
     void Clear()
@@ -28,43 +37,37 @@ class EventHungry : IPlayEvent
 {
     public DistractionController Distraction { get; set; }
     public EventSolutionHandler Solution { get; set; }
+    public string SceneName { get; set; }
 
     public string Play()
     {
+        SceneName = "MiniGameHungryScene";
         Distraction.StartHungry();
         return "[이벤트 발생!] 기아 바이러스가 전파되었습니다. 서둘러 급식소에서 배급을 받으세요!";
     }
-    public void MiniGame()
-    {
-        Distraction.gameObject.SetActive(false);
-        Solution.SolutionHungry();
-    }
-
 }
 class EventFog : IPlayEvent
 {
     public DistractionController Distraction { get; set; }
     public EventSolutionHandler Solution { get; set; }
-    
+    public string SceneName { get; set; }
+
     public string Play()
     {
+        SceneName = "MiniGameFogScene";
         Distraction.StartFog();
         return "[이벤트 발생!] 짙은 안개가 시야를 방해합니다. 치우러 가볼까요?";
     }
-    public void MiniGame()
-    {
-        Distraction.gameObject.SetActive(false);
-        Solution.SolutionFog();
-    }
-
 }
 class EventElec : IPlayEvent
 {
     public DistractionController Distraction { get; set; }
     public EventSolutionHandler Solution { get; set; }
-    
+    public string SceneName { get; set; }
+
     public string Play()
     {
+        SceneName = "MiniGameElecScene";
         Distraction.StartElec();
         return "[이벤트 발생!] 섬 내의 전기가 부족합니다. 발전소에서 전기를 생산해보아요!";
     }
@@ -79,9 +82,11 @@ class EventSaveNPC : IPlayEvent
 {
     public DistractionController Distraction { get; set; }
     public EventSolutionHandler Solution { get; set; }
-    
+    public string SceneName { get; set; }
+
     public string Play()
     {
+        SceneName = "MiniGameSaveNPCScene";
         Distraction.StartSaveNPC();
         return "[이벤트 발생!] 경감님이 돌무더기에 갇혔습니다. 경감님을 구하러 가세요!";
     }
--- Assets/Scripts/Play/Event/EventSolutionHandler.cs
+++ Assets/Scripts/Play/Event/EventSolutionHandler.cs
@@ -14,42 +14,22 @@ public class EventSolutionHandler : MonoBehaviour
 
         showBtnOnCollisionScript = GetComponent<ShowButtonOnCollision>();
         showBtnOnCollisionScript.enabled = true;
+        showBtnOnCollisionScript.ButtonToShow.GetComponentInChildren<Button>().onClick.RemoveAllListeners();
         showBtnOnCollisionScript.ButtonToShow.GetComponentInChildren<Button>().onClick.AddListener(delegate {
-            // Opening the minigame panel is done by ShowButtonOnCollision.cs
             NetworkManager.Instance.EventToPlay.MiniGame();
         });
-    }
-
-    public void SolutionHungry()
-    {
-        // 음식 배급 받기
-        // 홈즈 좌우로 움직여 떨어지는 음식 5개 먹기?
-    }
-    public void SolutionFog()
-    {
-        // 선풍기 돌리기
-        // 미니 선풍기 화면에서 움직이며 구름 오브젝트 치우기?
-    }
-    public void SolutionElec()
-    {
-        // 발전기 30번 때리기
-    }
-    public void SolutionSaveNPC()
-    {
-        // 돌 전부 세 번씩 두드려 깨기?
-        // EventSolved() Call
     }
 
     public void CloseGame()
     {
         NetworkManager.Instance.EventToPlay.CloseMiniGame(false);
-        showBtnOnCollisionScript.CanvasToShow.SetActive(false);
+        //showBtnOnCollisionScript.CanvasToShow.SetActive(false);
     }
     public void EventSolved()
     {
         NetworkManager.Instance.EventToPlay.CloseMiniGame(true);
         showBtnOnCollisionScript.ButtonToShow.SetActive(false);
-        showBtnOnCollisionScript.CanvasToShow.SetActive(false);
+        //showBtnOnCollisionScript.CanvasToShow.SetActive(false);
         SolutionSprite.sprite = EventDeactive;
         showBtnOnCollisionScript.enabled = false;
     }
